9c2afdbb6c3fc48d3c18990637df3be6d526b431
DeannaCarina <deannacarina@hotmail.com>
2023-02-15 16:22:05 +0000

    Import pandas, create variables, create output from input variables

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
+        
+# Install pandas in env with pip install pandas
 import csv
+import pandas as pd
 
-with open("example_abp_data.csv", "r") as file:
-  datalist = csv.reader(file)
-  for row in datalist:
-    row.append("test")
-    print(row)
+# Creating variables for the CSV data
+input_data = pd.read_csv('example_input_data.csv')
+abp_data = pd.read_csv('example_abp_data.csv')
+
+# Creating a new CSV with data from input_data
+with open('example_output_data.csv', 'w') as f:
+    data = pd.DataFrame(input_data)
+    data.to_csv("example_output_data.csv")
+    output_data = pd.read_csv('example_output_data.csv')
+    
 
 
 
-
+# print(input_data)
+# print(abp_data)
+print(output_data)
